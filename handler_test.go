b86f7a39d036a09bd920fdc64500acef026e63a3
Ignacio Saporiti <ignacio@saporiti.dev>
2023-07-21 14:16:49 +0200

    tests stock history handler

--- handler_test.go
+++ handler_test.go
@@ -2,6 +2,27 @@ package server
 
 import "testing"
 
+func TestHandleStockHistory(t *testing.T) {
+	t.Run("returns error if unknown ticker provided", func(t *testing.T) {
+		t.Parallel()
+		_, err := HandleStockHistory("UNKNOWN")
+		if err == nil {
+			t.Error("want an error but got nil")
+		}
+	})
+
+	t.Run("returns stock history for provided ticker", func(t *testing.T) {
+		t.Parallel()
+		stockHistory, err := HandleStockHistory("MSFT")
+		if err != nil {
+			t.Fatalf("want no error but got %s", err.Error())
+		}
+		if len(stockHistory) == 0 {
+			t.Errorf("want non-empty stock history, got empty")
+		}
+	})
+}
+
 func TestHandleUserStock(t *testing.T) {
 	t.Run("returns even tickers for testA", func(t *testing.T) {
 		t.Parallel()
